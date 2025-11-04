083a9aaed3ed3f90584b4ab66fe9926e3049e948
Your Name <ethan@EthansX13.localdomain>
2025-11-03 19:02:38 -0500

    please work

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'git@github.com:EthanC306/SWDEV.git',
+  base: '/SWDEV/',
   plugins: [react()],
 })
